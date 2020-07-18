1fad6ee9e833d1ba059a8c6abe4d58cb796e90d3
Xander Forrest <xander@xanderforrest.com>
2020-07-18 16:05:39 +0100

    Added websites page with Jinja templating

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ def services():
     return render_template('services.html')
 
 
+@app.route('/services/websites')
+def websites():
+    return render_template('services/websites.html')
+
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
