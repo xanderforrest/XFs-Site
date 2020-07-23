f1724e8f61767a1248f8cea077181bf6c89c5c74
Xander Forrest <xander@xanderforrest.com>
2020-07-23 10:40:15 +0100

    Started working on adding images into blog posts
    It could work better

--- static/app.js
+++ static/app.js
@@ -6,3 +6,7 @@ function copyContent() {
     document.getElementById("content").value = document.getElementById("contentEditor").innerHTML;
     return True;
 }
+
+function testFunc() {
+  document.getElementById("contentEditor").innerHTML += "<figure><img class='figure-img' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.fayettevilleflyer.com%2Fwp-content%2Fuploads%2F2015%2F09%2Fhozier.jpg&f=1&nofb=1' /><figcaption>Caption</figcaption></figure>"
+}
